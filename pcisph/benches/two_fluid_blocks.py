9e5ada75fe18727c3cfc8b28dc36afdf93f58e17
dineshadepu <adepu.dinesh.a@gmail.com>
2018-02-09 13:58:59 +0530

    1st bench in pcisph.

---
+++ pcisph/benches/two_fluid_blocks.py
@@ -0,0 +1,6 @@
+"""
+Two fluid blocks with opposite velocity collides.
+Simulated using PCISPH. Detail explanation can be found at
+corresponding sections of
+https://github.com/dineshadepu/report
+"""
